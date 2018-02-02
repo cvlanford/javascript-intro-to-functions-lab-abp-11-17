684be87335db613079e22c4354380c9e0d28117f
Caroline Lanford <caroline.lanford@gmail.com>
2018-02-02 18:27:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-    if (msg === "I love you, Grandma.") {
-        return "I love you, too."
-    } else if (msg === msg.toLowerCase()) {
-        return "I can\'t hear you!"
-    } else if (msg === msg.toUpperCase()) {
-        return "YES INDEED!"
+    if (string === `I love you, Grandma.`) {
+        return `I love you, too.`
+    } else if (string === string.toLowerCase()) {
+        return `I can't hear you!`
+    } else if (string === string.toUpperCase()) {
+        return `YES INDEED!`
     }
 }
